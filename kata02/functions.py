67a1159254003dcdd41d119a6b09ffb4a7a31011
David Talbot <david.talbot@pancentric.com>
2015-08-14 15:54:30 +0100

    This passes all of the test but I don't think it's very readable.

--- kata02/functions.py
+++ kata02/functions.py
@@ -1,3 +1,20 @@
-def chop(int, list):
-    pass
+def chop(number, array_of_numbers):
 
+    if number not in array_of_numbers:
+        return -1
+    else:
+        cropped_array = list(array_of_numbers)
+        crop_point = 0
+
+        while cropped_array:
+
+            if len(cropped_array) == 1:
+                return crop_point
+
+            half_point = len(cropped_array)/2
+
+            if number in cropped_array[:half_point]:
+                cropped_array = cropped_array[:half_point]
+            else:
+                cropped_array = cropped_array[half_point:]
+                crop_point += half_point
