1eb8e2b7aa4d503a7ce17593e4279d9604ca9450
David Talbot <david.talbot@pancentric.com>
2015-08-14 16:03:56 +0100

    Simplify.

--- kata02/functions.py
+++ kata02/functions.py
@@ -3,18 +3,13 @@ def chop(number, array_of_numbers):
     if number not in array_of_numbers:
         return -1
     else:
-        cropped_array = list(array_of_numbers)
         crop_point = 0
-
-        while cropped_array:
-
-            if len(cropped_array) == 1:
+        while array_of_numbers:
+            if len(array_of_numbers) == 1:
                 return crop_point
-
-            half_point = len(cropped_array)/2
-
-            if number in cropped_array[:half_point]:
-                cropped_array = cropped_array[:half_point]
+            half_point = len(array_of_numbers)/2
+            if number in array_of_numbers[:half_point]:
+                array_of_numbers = array_of_numbers[:half_point]
             else:
-                cropped_array = cropped_array[half_point:]
+                array_of_numbers = array_of_numbers[half_point:]
                 crop_point += half_point
