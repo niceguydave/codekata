2cdb04e38ac8f5b36eebe17a18ecdce9f4797212
David Talbot <david.talbot@pancentric.com>
2015-08-17 21:54:34 +0100

    Validate the input a bit more

--- kata04/functions.py
+++ kata04/functions.py
@@ -9,7 +9,17 @@ def checkFileIsReadable(filepath):
         open(filepath)
     except IOError, OSError:  # Note OSError is for later versions of python
         raise IOError("Unable to read the supplied file '{}'".format(filepath))
+    return True
 
 
-def processWeather(filename):
-    checkFileIsReadable(filename)
+def checkFileHasValidSuffix(filepath):
+    valid_extension = '.dat'
+    filename, file_extension = os.path.splitext(filepath)
+    if file_extension != valid_extension:
+        raise ValueError("Only {} files are valid.")
+    return True
+
+
+def processWeather(filepath):
+    checkFileIsReadable(filepath)
+    checkFileHasValidSuffix(filepath)
--- kata04/tests.py
+++ kata04/tests.py
@@ -1,21 +1,35 @@
 import unittest
 import os
 
-from functions import checkFileIsReadable
+from functions import checkFileIsReadable, checkFileHasValidSuffix
 
 
 class WeatherTest(unittest.TestCase):
 
-    __location__ = os.path.realpath(os.path.join(os.getcwd(),
+    __this_directory__ = os.path.realpath(os.path.join(os.getcwd(),
                                                  os.path.dirname(__file__)))
 
     def setUp(self):
-        self.dat_file = os.path.join(self.__location__, 'weather.dat')
+        self.dat_file = os.path.join(self.__this_directory__, 'weather.dat')
 
     def test_missing_file_raises_io_error(self):
-        self.nonsense_dat = os.path.join(self.__location__, 'not-here.dat')
+        self.nonsense_dat = os.path.join(self.__this_directory__,
+                                         'not-here.dat')
         self.assertFalse(os.path.exists(self.nonsense_dat))
         self.assertRaises(IOError, checkFileIsReadable, self.nonsense_dat)
+
+    def test_valid_file_recognised_as_readable(self):
+        self.assertTrue(checkFileIsReadable(self.dat_file))
+
+    def test_non_dat_file_raises_value_error(self):
+        """Some sample file extensions here - this list could be expanded.
+        """
+        bad_filepaths = ['file.xls', 'file.csv', 'file.doc', ]
+        for bad_file in bad_filepaths:
+            self.assertRaises(ValueError, checkFileHasValidSuffix, bad_file)
+
+    def test_dat_file_recognised_as_valid(self):
+        self.assertTrue(checkFileHasValidSuffix(self.dat_file))
 
 
 if __name__ == '__main__':
